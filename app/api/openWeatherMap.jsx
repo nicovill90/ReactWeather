78db40645ee267567066b96827d991efe8587a6c
nicovill90 <nicovillalonga90@hotmail.com>
2016-12-16 18:25:07 -0300

    add error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -8,7 +8,7 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function (res) {
-      if (res.data.cod && res.data.message) {
+      if (res.data.cod && res.data.message || res.data.name === "Bokod") {
         throw new Error(res.data.message);
       } else {
         return res.data.main.temp;
